261a03f10651b4e8cde78266da537df30efd035e
Pergamene <smartz373@gmail.com>
2020-07-11 16:57:52 -0600

    lookup table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,26 +1,37 @@
-# Your code here
-
+import math
+import random
 
 def slowfun_too_slow(x, y):
+  v = math.pow(x, y)
+  v = math.factorial(v)
+  v //= (x + y)
+  v %= 982451653
+
+  return v
+
+def _make_coord_str(x, y):
+  return f'{x},{y}'
+
+lookup_table = {}
+
+def slowfun(x, y):
+  """
+  Rewrite slowfun_too_slow() in here so that the program produces the same
+  output, but completes quickly instead of taking ages to run.
+  """
+  # Your code here
+  v = lookup_table.get(_make_coord_str(x, y))
+  if v is None:
     v = math.pow(x, y)
     v = math.factorial(v)
     v //= (x + y)
     v %= 982451653
-
-    return v
-
-def slowfun(x, y):
-    """
-    Rewrite slowfun_too_slow() in here so that the program produces the same
-    output, but completes quickly instead of taking ages to run.
-    """
-    # Your code here
-
+    lookup_table[_make_coord_str(x, y)] = v
+  return v
 
 
 # Do not modify below this line!
-
 for i in range(50000):
-    x = random.randrange(2, 14)
-    y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+  x = random.randrange(2, 14)
+  y = random.randrange(3, 6)
+  print(f'{i}: {x},{y}: {slowfun(x, y)}')
